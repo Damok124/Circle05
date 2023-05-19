9056ff920dc80d165f27e07cd5faa0dc559ed811
Zied Harzi <zharzi@student.42angouleme.fr>
2023-05-19 23:15:22 +0200

    19may23

---
+++ CPP/Module06/ex00/ScalarConverter.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zharzi <zharzi@student.42angouleme.fr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/19 23:09:24 by zharzi            #+#    #+#             */
+/*   Updated: 2023/05/19 23:13:00 by zharzi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+# define SCALARCONVERTER_HPP
+
+# include <iostream>
+
+class ScalarConverter
+{
+	private :
+	public :
+};
+
+#endif
+
+// Write a static class ScalarConverter that will contain a method "convert" takes as
+// parameter a string representation of a C++ literal in its most common form. This literal
+// must belong to one of the following a scalar types:
+// • char
+// • int
+// • float
+// • double
+// Except for char parameters, only the decimal notation will be used.
+// Examples of char literals: ’c’, ’a’, ...
+// To make things simple, please note that non displayable characters shouldn’t be used as
+// inputs. If a conversion to char is not displayable, prints an informative message.
+// Examples of int literals: 0, -42, 42...
+// Examples of float literals: 0.0f, -4.2f, 4.2f...
+// You have to handle these pseudo literals as well (you know, for science): -inff, +inff
+// and nanf.
+// Examples of double literals: 0.0, -4.2, 4.2...
+// You have to handle these pseudo literals as well (you know, for fun): -inf, +inf and nan.
---
+++ CPP/Module06/ex00/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zharzi <zharzi@student.42angouleme.fr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/19 23:04:15 by zharzi            #+#    #+#             */
+/*   Updated: 2023/05/19 23:12:21 by zharzi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "ScalarConverter.hpp"
+
+int main(void)
+{
+	return (0);
+}
+
+// Write a program to test that your class works as expected.
+// You have to first detect the type of the literal passed as parameter, convert it from
+// string to its actual type, then convert it explicitly to the three other data types.
+// Lastly, display the results as shown below.
+// If a conversion does not make any sense or overflows, display a message to inform
+// the user that the type conversion is impossible. Include any header you need in order to
+// handle numeric limits and special values.
