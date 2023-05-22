7f7462bdfb4785796c7013c000f4693a7feb4a5b
Damok124VM <zharzi@student.42angouleme.fr>
2023-05-22 11:36:45 +0200

    22may23

--- CPP/Module06/ex00/ScalarConverter.cpp
+++ CPP/Module06/ex00/ScalarConverter.cpp
@@ -6,9 +6,11 @@
 /*   By: zharzi <zharzi@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 23:10:57 by zharzi            #+#    #+#             */
-/*   Updated: 2023/05/19 23:11:12 by zharzi           ###   ########.fr       */
+/*   Updated: 2023/05/22 11:36:29 by zharzi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
+// Allowed functions : Any function to convert from a string to an int,
+// a float or a double. This will help, but won’t do the whole job.s
