22452e2e32ff0c64f6517efc3327a70dfa8901ca
Zied Harzi <zharzi@student.42angouleme.fr>
2023-05-23 21:54:45 +0200

    23may23

--- CPP/Module06/ex00/ScalarConverter.cpp
+++ CPP/Module06/ex00/ScalarConverter.cpp
@@ -6,9 +6,13 @@
 /*   By: zharzi <zharzi@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 23:10:57 by zharzi            #+#    #+#             */
-/*   Updated: 2023/05/19 23:11:12 by zharzi           ###   ########.fr       */
+/*   Updated: 2023/05/23 21:53:03 by zharzi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
+void	ScalarConverter::convert(std::string const literal)
+{
+	(void)literal;
+}
--- CPP/Module06/ex00/main.cpp
+++ CPP/Module06/ex00/main.cpp
@@ -6,22 +6,51 @@
 /*   By: zharzi <zharzi@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 23:04:15 by zharzi            #+#    #+#             */
-/*   Updated: 2023/05/19 23:12:21 by zharzi           ###   ########.fr       */
+/*   Updated: 2023/05/23 21:53:05 by zharzi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "ScalarConverter.hpp"
 
-int main(void)
+int main(int ac, char** argv)
 {
+	if (ac == 2)
+	{
+		char	c;
+		int		i;
+		float	f;
+		double	d;
+		i = str.atoi(argv[1])
+	}
 	return (0);
 }
 
 // Write a program to test that your class works as expected.
-// You have to first detect the type of the literal passed as parameter, convert it from
-// string to its actual type, then convert it explicitly to the three other data types.
+
+// You have to first detect the type of the literal passed as parameter,
+// convert it from string to its actual type,
+// then convert it explicitly to the three other data types.
+
 // Lastly, display the results as shown below.
-// If a conversion does not make any sense or overflows, display a message to inform
-// the user that the type conversion is impossible. Include any header you need in order to
-// handle numeric limits and special values.
+
+// If a conversion does not make any sense or overflows,
+// display a message to inform the user that the type conversion is impossible.
+// Include any header you need in order to handle numeric limits and special values.
+
+//	shell :
+// ./convert 0
+// char: Non displayable
+// int: 0
+// float: 0.0f
+// double: 0.0
+// ./convert nan
+// char: impossible
+// int: impossible
+// float: nanf
+// double: nan
+// ./convert 42.0f
+// char: '*'
+// int: 42
+// float: 42.0f
+// double: 42.0
