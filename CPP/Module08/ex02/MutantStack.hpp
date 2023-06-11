7c8eb4d634453f2ad52d58986eba8c2f454df0b8
Damok124VM <zharzi@student.42angouleme.fr>
2023-06-11 09:23:08 +0200

    11jun23

--- CPP/Module08/ex02/MutantStack.hpp
+++ CPP/Module08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: zharzi <zharzi@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/10 17:46:00 by zharzi            #+#    #+#             */
-/*   Updated: 2023/06/10 23:58:19 by zharzi           ###   ########.fr       */
+/*   Updated: 2023/06/11 09:21:36 by zharzi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,51 +17,19 @@
 # include <stack>
 # include <list>
 
-// Pour réparer cette injustice, vous devez rendre le conteneur std::stack itérable.
-
-// Écrivez une classe MutantStack. Elle sera implémentée à partir d'un std::stack.
 template<typename T>
 class MutantStack : public std::stack<T>
 {
 	public :
-		MutantStack() {}
-		MutantStack(MutantStack const& source) {(void)source;}
-		MutantStack& operator=(MutantStack const& source) {(void)source; return (*this);}
-		~MutantStack() {}
 		typedef typename std::stack<T>::container_type::iterator iterator;
+
+		MutantStack() {}
+		MutantStack(MutantStack const& source) : std::stack<T>(source) {}
+		MutantStack& operator=(MutantStack const& source) {if (this == &source) return (*this); return (MutantStack(source));}
+		~MutantStack() {}
+
 		iterator begin(void) {return (std::stack<T>::c.begin());}
 		iterator end(void) {return (std::stack<T>::c.end());}
 };
 
 #endif
-
-// Elle offrira toutes les fonctions membres de celui-ci, ainsi qu'une
-// fonctionnalité supplémentaire : des itérateurs.
-// Bien sûr, vous écrirez et soumettrez vos propres tests pour vous assurer que tout
-// fonctionne comme prévu.
-
-
-
-
-// Si vous l'exécutez une première fois avec votre MutantStack, puis une deuxième fois
-// en remplaçant le MutantStack par, par exemple, un std::list, les deux sorties devraient
-// être identiques. Bien sûr, lorsque vous testez un autre conteneur, mettez à jour le code
-// ci-dessous avec les fonctions membres correspondantes (push() peut devenir push_back(), par exemple).
-
-
-
-// Now, time to move on more serious things. Let’s develop something weird.
-// The std::stack container is very nice. Unfortunately, it is one of the only STL Con-
-// tainers that is NOT iterable. That’s too bad.
-// But why would we accept this? Especially if we can take the liberty of butchering the
-// original stack to create missing features.
-// To repair this injustice, you have to make the std::stack container iterable.
-// Write a MutantStack class. It will be implemented in terms of a std::stack.
-// It will offer all its member functions, plus an additional feature: iterators.
-// Of course, you will write and turn in your own tests to ensure everything works as
-// expected.
-
-// If you run it a first time with your MutantStack, and a second time replacing the
-// MutantStack with, for example, a std::list, the two outputs should be the same. Of
-// course, when testing another container, update the code below with the corresponding
-// member functions (push() can become push_back()).
--- CPP/Module08/ex02/main.cpp
+++ CPP/Module08/ex02/main.cpp
@@ -6,38 +6,12 @@
 /*   By: zharzi <zharzi@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/10 17:31:47 by zharzi            #+#    #+#             */
-/*   Updated: 2023/06/10 23:57:57 by zharzi           ###   ########.fr       */
+/*   Updated: 2023/06/11 09:21:22 by zharzi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "MutantStack.hpp"
-
-// int main()
-// {
-// 	MutantStack<int> mstack;
-// 	mstack.push(5);
-// 	mstack.push(17);
-// 	std::cout << mstack.top() << std::endl;
-// 	mstack.pop();
-// 	std::cout << mstack.size() << std::endl;
-// 	mstack.push(3);
-// 	mstack.push(5);
-// 	mstack.push(737);
-// 	//[...]
-// 	mstack.push(0);
-// 	MutantStack<int>::iterator it = mstack.begin();
-// 	MutantStack<int>::iterator ite = mstack.end();
-// 	++it;
-// 	--it;
-// 	while (it != ite)
-// 	{
-// 	std::cout << *it << std::endl;
-// 	++it;
-// 	}
-// 	std::stack<int> s(mstack);
-// 	return 0;
-// }
 
 int main()
 {
