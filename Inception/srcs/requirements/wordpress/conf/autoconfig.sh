995123151ef4ef163f95e6984a08b9a2d8189bc0
z harzi <zharzi@student.42angouleme.fr>
2023-07-28 12:24:21 +0200

    28jul23

--- Inception/srcs/requirements/wordpress/conf/autoconfig.sh
+++ Inception/srcs/requirements/wordpress/conf/autoconfig.sh
@@ -1,11 +1,21 @@
 sleep 10
 
+# wp core download    --allow-root --path='/var/www/wordpress'
+
 wp config create	--allow-root \
                     --dbname=$SQL_DATABASE \
                     --dbuser=$SQL_USER \
                     --dbpass=$SQL_PASSWORD \
                     --dbhost=mariadb:3306 --path='/var/www/wordpress'
 
-# wp core install
-# sudo -u www-data wp core install --url='http://example.com' --title='Blog Title' --admin_user='adminuser' --admin_password='password' --admin_email='email@domain.com'
-# wp user create
+wp core install --allow-root \
+                --url='https://zharzi.42.fr.com' \
+                --title='Simpliest Wordpress Ever' \
+                --admin_user='admin' \
+                --admin_password='password' \
+                --admin_email='zharzi@student.42angouleme.fr' \
+                --path='/var/www/wordpress'
+
+wp user create --allow-root new_user new_user@zharzi.42.fr --role=subscriber --path='/var/www/wordpress'
+
+/usr/sbin/php-fpm7.3 -F 
