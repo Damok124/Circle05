834c59f6be9ed96337b92b97cf16fc155f7116f6
z harzi <zharzi@student.42angouleme.fr>
2023-08-09 19:57:40 +0200

    09aug23

---
+++ Inception/srcs/requirements/wordpress/tools/autoconfig.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd /var/www/wordpress;
+
+wp core download    --allow-root
+
+wp config create	--allow-root \
+                    --dbname=$DB \
+                    --dbuser=$DB_USER \
+                    --dbpass=$DB_PASSWORD \
+                    --dbhost='mariadb'
+
+wp core install --allow-root \
+                --url='https://'$DOMAIN_NAME \
+                --title='Ziedest Wordpress Ever' \
+                --admin_user=$ADMIN_USER \
+                --admin_password=$ADMIN_PASSWORD \
+                --admin_email=$ADMIN_EMAIL
+            
+wp user create --allow-root "$USER_NAME" "$USER_NAME@$DOMAIN_NAME" --role=subscriber
+
+/usr/sbin/php-fpm7.3 -F -R
